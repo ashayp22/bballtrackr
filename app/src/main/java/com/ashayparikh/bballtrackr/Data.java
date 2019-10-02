fdb6fbdc6567ec988d9551cc55e04f32fa31a82e
ashayp22 <ashayparikh22@gmail.com>
2019-10-01 22:12:27 -0500

    found the link for storing data

---
+++ app/src/main/java/com/ashayparikh/bballtrackr/Data.java
@@ -0,0 +1,14 @@
+package com.ashayparikh.bballtrackr;
+
+import java.io.FileOutputStream;
+
+public abstract class Data {
+
+//https://www.journaldev.com/9383/android-internal-storage-example-tutorial
+
+    public static void CreateFile() {
+
+    }
+
+
+}
