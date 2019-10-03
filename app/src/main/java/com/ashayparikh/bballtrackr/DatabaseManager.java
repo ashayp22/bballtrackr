02cf25d660b36ebb39c9d2123b24a3bfae6d12a5
ashayp22 <ashayparikh22@gmail.com>
2019-10-03 18:21:49 -0500

    sql database basics done

--- app/src/main/java/com/ashayparikh/bballtrackr/DatabaseManager.java
+++
@@ -1,4 +0,0 @@
-package com.ashayparikh.bballtrackr;
-
-public class DatabaseManager {
-}
--- app/src/main/java/com/ashayparikh/bballtrackr/MainActivity.java
+++ app/src/main/java/com/ashayparikh/bballtrackr/MainActivity.java
@@ -1,14 +1,44 @@
 package com.ashayparikh.bballtrackr;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.database.Cursor;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private DatabaseHelper db;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        db = new DatabaseHelper(this);
     }
+
+
+    public void viewAll() {
+        Cursor res = db.getAllData();
+        if(res.getCount() == 0) {
+            //no players
+            return;
+        }
+
+        StringBuffer buffer = new StringBuffer();
+        while (res.moveToNext()) {
+            buffer.append("Name :"+ res.getString(0)+"\n");
+            buffer.append("Number :"+ res.getString(1)+"\n");
+            buffer.append("Points :"+ res.getString(2)+"\n");
+            buffer.append("Assists :"+ res.getString(3)+"\n\n");
+        }
+
+    }
+
+
+
+    //clear player data, team stats
+
 }
