09ae26b82c7022ae0b76778a5490e2f7b263097c
ashayp22 <ashayparikh22@gmail.com>
2019-10-02 22:48:51 -0500

    wrote class

--- app/src/main/java/com/ashayparikh/bballtrackr/Data.java
+++ app/src/main/java/com/ashayparikh/bballtrackr/Data.java
@@ -1,14 +1,46 @@
 package com.ashayparikh.bballtrackr;
 
 import java.io.FileOutputStream;
+import java.io.File;
+import android.content.Context;
 
-public abstract class Data {
+public class Data {
 
 //https://www.journaldev.com/9383/android-internal-storage-example-tutorial
 
-    public static void CreateFile() {
+    public Data(String filename) {
 
     }
 
+    //creates a file
+    public static void CreateFile(String filename, Context context) {
+        FileOutputStream fOut = context.openFileOutput(filename, Context.MODE_PRIVATE);
+    }
+
+    //checks if a file exists
+    public boolean FileExists(String filepath) {
+        File file = new File(filepath);
+        return file.exists();
+    }
+
+    //returns a string containing the data of a player
+    public String getPlayerData(int player) {
+
+    }
+
+    //parses the text file, returns all data in string format
+    public String getData() {
+
+    }
+
+    //adds a player to the team
+    public void AddPlayer() {
+
+    }
+
+    //modify stats of a certain player
+    public void ModifyPlaterStats(int[] data) {
+
+    }
 
 }
